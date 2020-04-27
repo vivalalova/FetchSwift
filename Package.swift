71e21c16124b60dcc8db3b6f25f41d64ac4d6bd7
vivalalova <vivalalova0@gmail.com>
2020-04-28 00:15:51 +0800

    ~

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "FetchSwift",
     platforms: [
         .iOS(SupportedPlatform.IOSVersion.v13),
+        .macOS(SupportedPlatform.MacOSVersion.v10_15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
